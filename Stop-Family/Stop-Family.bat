072d0208e579a877158ee8ac749c208e7d085f51
Jay Jackson <thebotlynoob@gmail.com>
2021-10-08 17:57:33 -0400

    Update Stop-Family.bat

--- Stop-Family/Stop-Family.bat
+++ Stop-Family/Stop-Family.bat
@@ -1,7 +1,6 @@
 @echo off
 
-cd /d "%~dp0" && ( if exist "%temp%\getadmin.vbs" del "%temp%\getadmin.vbs" ) && fsutil dirty query %systemdrive% 1>nul 2>nul || (  echo Set UAC = CreateObject^("Shell.Application"^) : UAC.ShellExecute "cmd.exe", "/k cd ""%~sdp0"" && %~s0 %*", "", "runas", 1 >> "%temp%\getadmin.vbs" && "%temp%\getadmin.vbs" && exit /B )
+cd /d "%~dp0" && ( if exist "%temp%\getadmin.vbs" del "%temp%\getadmin.vbs" ) && fsutil dirty query %systemdrive% 1>nul 2>nul||(  echo Set UAC=CreateObject^("Shell.Application"^):UAC.ShellExecute "cmd.exe","/k cd ""%~sdp0"" && %~s0","","runas", 1 >>"%temp%\getadmin.vbs" && "%temp%\getadmin.vbs"&& exit /B )
+if "%1" == "" start "" /min "%~f0" MINIMIZED && exit
 
-:loop
-taskkill /f /im WpcMon.exe
-goto loop
+for /L %%N in () do @taskkill /f /im WpcMon.exe
