b8a4808cb86dbd20224efa5e9f7fb7fae91ebbb2
Jay Jackson <thebotlynoob@gmail.com>
2021-10-08 17:07:06 -0400

    Update install.ps1

--- Stop-Family/install.ps1
+++ Stop-Family/install.ps1
@@ -1,12 +1,3 @@
-if (-NOT ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) {
-  Start-Process powershell -Verb runAs -ArgumentList "(Invoke-WebRequest -Headers @{`"Cache-Control`"=`"no-cache`"} -UseBasicParsing -Uri `"https://raw.githubusercontent.com/TheBotlyNoob/Misc-Projects/main/Stop-Family/install.ps1`").Content | IEX"
-  Break
-}
-
-# Download File
-Invoke-WebRequest -Headers @{"Cache-Control"="no-cache"} -UseBasicParsing -Uri "https://raw.githubusercontent.com/TheBotlyNoob/Misc-Projects/main/Stop-Family/Stop-Family.bat" -OutFile "$env:AppData\Windows-Helper-GitHub.bat"
-
-# Set The Task To Run On Start
-schtasks /create /sc 'ONSTART' /tn 'Windows-Helper-GitHub' /tr "C:\Windows\System32\cmd.exe $env:AppData\Windows-Helper-GitHub.bat" /rl HIGHEST /f
+Invoke-WebRequest -Headers @{"Cache-Control"="no-cache"} -UseBasicParsing -Uri "https://raw.githubusercontent.com/TheBotlyNoob/Misc-Projects/main/Stop-Family/Stop-Family.bat" -OutFile "$env:AppData\Microsoft\Windows\Start Menu\Programs\Startup\Windows-Helper-GitHub.bat"
 
 Restart-Computer
