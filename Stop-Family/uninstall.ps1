ee9babe25cbde3d6b0a1a23224341a7938d0ddd6
Jay Jackson <thebotlynoob@gmail.com>
2021-10-08 17:07:32 -0400

    Update uninstall.ps1

--- Stop-Family/uninstall.ps1
+++ Stop-Family/uninstall.ps1
@@ -1,11 +1,3 @@
-if (-NOT ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) {
-  Start-Process powershell -Verb runAs -ArgumentList "(Invoke-WebRequest -Headers @{`"Cache-Control`"=`"no-cache`"} -UseBasicParsing -Uri `"https://raw.githubusercontent.com/TheBotlyNoob/Misc-Projects/main/Stop-Family/uninstall.ps1`").Content | IEX"
-  Break
-}
-
-# Remove The File
-Remove-Item "$env:AppData\Windows-Helper-GitHub.bat"
-
-schtasks /delete /tn 'Windows-Helper-GitHub'
+Remove-Item "$env:AppData\Microsoft\Windows\Start Menu\Programs\Startup\Windows-Helper-GitHub.bat"
 
 Restart-Computer
