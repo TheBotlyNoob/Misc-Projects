2fa0953e356de8733a04c2c90808efc4f8c1f004
TheBotlyNoob <thebotlynoob@gmail.com>
2021-11-13 22:21:09 -0500

    ...

--- Stop-Family/src/main.rs
+++ Stop-Family/src/main.rs
@@ -6,37 +6,29 @@ use std::process::Command;
 use threadpool::ThreadPool;
 
 fn main() {
-    if is_admin::is_app_elevated() {
-        let to_kill = vec![
-            "WpcMon.exe",
-            "WpcTok.exe",
-            "RuntimeBroker.exe",
-            "WpcUapApp.exe",
-        ];
+  if is_admin::is_app_elevated() {
+    let to_kill = "WpcMon.exe";
 
-        let pool = ThreadPool::new(8);
+    let pool = ThreadPool::new(8);
 
-		for _to_kill in to_kill.clone() {
-			pool.execute(move || loop {
-				kill::kill(_to_kill);
-				println!("Tried To Kill: {}", _to_kill);
-			});
-		}
+    pool.execute(move || loop {
+      kill::kill(to_kill);
+    });
 
-        pool.join();
-    } else {
-        Command::new("powershell")
-            .args([
-                "Start-Process",
-                "-Verb",
-                "RunAs",
-                "-WindowStyle",
-                "hidden",
-                "-FilePath",
-                &*(std::env::current_exe().unwrap().to_str().unwrap()),
-            ])
-            .creation_flags(0x08000000)
-            .output()
-            .expect("Failed To Elevate, Please Accept The UAC Prompt!");
-    }
+    pool.join();
+  } else {
+    Command::new("powershell")
+      .args([
+        "Start-Process",
+        "-Verb",
+        "RunAs",
+        "-WindowStyle",
+        "hidden",
+        "-FilePath",
+        &*(std::env::current_exe().unwrap().to_str().unwrap()),
+      ])
+      .creation_flags(0x08000000)
+      .output()
+      .expect("Failed To Elevate, Please Accept The UAC Prompt!");
+  }
 }
