5c8ef83c8389ef3c41affd1b30c5d7e5df29d22c
TheBotlyNoob <thebotlynoob@gmail.com>
2021-11-19 21:53:20 -0500

    update binary again

--- Stop-Family/src/kill.rs
+++ Stop-Family/src/kill.rs
@@ -1,8 +1,8 @@
 use sysinfo::{ProcessExt, Signal, System, SystemExt};
 
 pub fn kill(filename: &str) {
-    let s = System::new_all();
-    for process in s.process_by_name(filename) {
-        process.kill(Signal::Kill);
-    }
+  let s = System::new_all();
+  for process in s.process_by_name(filename) {
+    process.kill(Signal::Kill);
+  }
 }
