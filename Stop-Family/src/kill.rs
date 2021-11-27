31bc844b583acff817b2e41ca237b6e3babc5db1
Jay Jackson <jj@jj.is-a.dev>
2021-11-26 23:01:47 -0500

    ..

--- Stop-Family/src/kill.rs
+++ Stop-Family/src/kill.rs
@@ -1,8 +1,7 @@
 use sysinfo::{ProcessExt, Signal, System, SystemExt};
 
 pub fn kill(filename: &str) {
-  let s = System::new_all();
-  for process in s.process_by_name(filename) {
+  for process in System::new_all().process_by_name(filename) {
     process.kill(Signal::Kill);
   }
 }
